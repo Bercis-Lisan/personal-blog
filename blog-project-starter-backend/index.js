0ad7fb152192b9fb0f38200d9e46a71729e20c27
Bercis-Lisan <bercislisan569@gmail.com>
2026-09-16 13:02:08 +0530

    initial push

--- blog-project-starter-backend/index.js
+++ blog-project-starter-backend/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const cors = require("cors")
 
 const app = express();
-app.use(cors())
+app.use(cors({origin:`${process.env.CORS_ORIGIN}`}))
 
 // Middleware
 app.use(bodyParser.json());
